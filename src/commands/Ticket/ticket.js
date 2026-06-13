ae93085c595833e72e727e1eff3077f35789130c
Hx <arthurramos16021@gmail.com>
2026-06-13 17:03:36 -0300

    Update ticket.js

--- src/commands/Ticket/ticket.js
+++ src/commands/Ticket/ticket.js
@@ -153,7 +153,7 @@ const panelMessage = interaction.options.getString("panel_message") || "Click th
 const dmOnClose = interaction.options.getBoolean("dm_on_close") !== false;
 
             const setupEmbed = createEmbed({ 
-                title: "🎫 Support Tickets", 
+                title: "📩 Central de Atendimento - Os pecinhas 📩", 
 description: panelMessage,
                 color: getColor('info')
             });
